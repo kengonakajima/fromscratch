0d09426f3627ed7b884742ebd676d832fe73dd40
kengo nakajima <kengo.nakajima@gmail.com>
2025-08-19 14:02:07 +0900

    up

---
+++ ch02_05.py
@@ -0,0 +1,5 @@
+import re
+text = "Hello, world. This, is a test."
+result = re.split(r'([,.]|\s)', text)
+
+print(result)
---
+++ ch02_06.py
@@ -0,0 +1,6 @@
+import re
+text = "Hello, world. This, is a test."
+result = re.split(r'([,.]|\s)', text)
+result = [item for item in result if item.strip()]
+
+print(result)
