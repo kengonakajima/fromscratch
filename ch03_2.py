c425909a01cca4732e2278e007565292aba3a4ea
kengo nakajima <kengo.nakajima@gmail.com>
2025-08-20 13:38:28 +0900

    3.5

--- ch03_2.py
+++ ch03_2.py
@@ -348,4 +348,66 @@ print(dropout(example)) # ランダムな2と0からなる6x6行列
 torch.manual_seed(123)
 print(dropout(attn_weights)) #なんか合計が1越えてる
 
+# 3.5.3 Implementing a compact causal self-attention class
+# casual じゃなくて、 causalだった。。因果的。
+# causalマスク(右上0)　と、ドロップアウトマスクを適用した。
+
+# バッチを作る必要がある。
+
+
+# [33] まず単純に同じのを重ねてみる
+
+batch = torch.stack((inputs, inputs), dim=0)
+print(batch.shape) # 2 inputs with 6 tokens each, and each token has embedding dimension 3
+
+
+
+
+# [34] classにまとめる
+
+class CausalAttention(nn.Module):
+
+    def __init__(self, d_in, d_out, context_length,
+                 dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout) # New
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1)) # New
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape # New batch dimension b
+        # For inputs where `num_tokens` exceeds `context_length`, this will result in errors
+        # in the mask creation further below.
+        # In practice, this is not a problem since the LLM (chapters 4-7) ensures that inputs  
+        # do not exceed `context_length` before reaching this forward method. 
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2) # Changed transpose
+        attn_scores.masked_fill_(  # New, _ ops are in-place
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)  # `:num_tokens` to account for cases where the number of tokens in the batch is smaller than the supported context_size
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights) # New
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+torch.manual_seed(123)
+
+context_length = batch.shape[1]
+ca = CausalAttention(d_in, d_out, context_length, 0.0)
+
+context_vecs = ca(batch)
+
+print(context_vecs) # とりあえず2つ出る
+print("context_vecs.shape:", context_vecs.shape)
+
+# Note that dropout is only applied during training, not during inference
+# droopoutは学習中のみなんだね
 
