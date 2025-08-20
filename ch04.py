8df0979799cc4e8fcea2705881e2b9aea2db25b2
kengo nakajima <kengo.nakajima@gmail.com>
2025-08-20 16:36:32 +0900

    4.2

--- ch04.py
+++ ch04.py
@@ -9,3 +9,179 @@ print("matplotlib version:", version("matplotlib"))
 print("torch version:", version("torch"))
 print("tiktoken version:", version("tiktoken"))
 
+# [2] ここでは124mのGPT2に準拠した設定をする
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size, BPE, in ch02
+    "context_length": 1024, # Context length モデルのインプット xの最大長
+    "emb_dim": 768,         # Embedding dimension 
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers  この後説明する
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias q,k,vを計算するときに使うバイアス。 ch5で説明。
+}
+
+
+# text > tokens > token ids > token embeddings (768) > GPT MODEL > output vectors (768) > postprocess > text
+
+# [3]
+import torch
+import torch.nn as nn
+
+
+class DummyGPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        
+        # Use a placeholder for TransformerBlock
+        self.trf_blocks = nn.Sequential(
+            *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+        
+        # Use a placeholder for LayerNorm
+        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+
+class DummyTransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        # A simple placeholder
+
+    def forward(self, x):
+        # This block does nothing and just returns its input.
+        return x
+
+
+class DummyLayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-5):
+        super().__init__()
+        # The parameters here are just to mimic the LayerNorm interface.
+
+    def forward(self, x):
+        # This layer does nothing and just returns its input.
+        return x
+
+    
+# [4]
+
+import tiktoken
+import torch
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+batch = []
+
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
+# [5]
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+
+logits = model(batch)
+print("Output shape:", logits.shape)
+print(logits)
+
+
+# 4.2 Normalizing activations with layer normalization
+
+# Layer normalization, also known as LayerNorm (Ba et al. 2016), centers the activations of a neural network layer around a mean of 0 and normalizes their variance to 1
+# This stabilizes training and enables faster convergence to effective weights
+# Layer normalization is applied both before and after the multi-head attention module within the transformer block, which we will implement later; it's also applied before the final output layer
+
+# NN層の活性化を、平均0、分散1に正規化する。こうすることで安定化する。
+
+# [6] Let's see how layer normalization works by passing a small input sample through a simple neural network layer:
+
+torch.manual_seed(123)
+
+# create 2 training examples with 5 dimensions (features) each
+batch_example = torch.randn(2, 5) 
+
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+print(out)
+
+# [7] Let's compute the mean and variance for each of the 2 inputs above:
+
+
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+
+print("Mean:\n", mean)  # 平均
+print("Variance:\n", var) # 分差
+
+# [8] Subtracting the mean and dividing by the square-root of the variance (standard deviation) centers the inputs to have a mean of 0 and a variance of 1 across the column (feature) dimension:
+
+out_norm = (out - mean) / torch.sqrt(var)
+print("Normalized layer outputs:\n", out_norm)
+
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+
+# [9] Each input is centered at 0 and has a unit variance of 1; to improve readability, we can disable PyTorch's scientific notation:
+
+torch.set_printoptions(sci_mode=False)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+# Above, we normalized the features of each input
+# [10] Now, using the same idea, we can implement a LayerNorm class:
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5 # 分散が0のときにゼロ除算エラーを防ぐため
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+    
+# 平均を引いて分散で割る正規化処理に加えて、学習可能な2つのパラメータ（スケールとシフト） を導入しています。
+#スケール: 正規化後の値に掛け算する係数（初期値は1）
+#シフト: 正規化後の値に足すバイアス（初期値は0）
+
+# [11] Let's now try out LayerNorm in practice:
+
+
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+
+# [12]
+
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
