a571bdb2b3eb8328f590276d6c3feb6a0ffbbbd9
kengo nakajima <kengo.nakajima@gmail.com>
2025-08-20 14:12:55 +0900

    4

---
+++ ch04.py
@@ -0,0 +1,11 @@
+# Chapter 4: Implementing a GPT model from Scratch To Generate Text
+# 4章はGPTみたいなやつをつくる。次の章はそれをTrainする
+
+# [1]
+
+from importlib.metadata import version
+
+print("matplotlib version:", version("matplotlib"))
+print("torch version:", version("torch"))
+print("tiktoken version:", version("tiktoken"))
+
