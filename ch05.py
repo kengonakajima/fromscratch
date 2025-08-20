8737359ec8af56ee1c321b7e90e89581e6ba2478
kengo nakajima <kengo.nakajima@gmail.com>
2025-08-20 21:38:57 +0900

    5..

--- ch05.py
+++ ch05.py
@@ -187,15 +187,181 @@ class GPTModel(nn.Module):
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,   # Vocabulary size
-    "context_length": 256, # Shortened context length (orig: 1024)
+    "context_length": 256, # Shortened context length (orig GPT2: 1024) メモリが多いなら1024にしてもいい. 
     "emb_dim": 768,        # Embedding dimension
     "n_heads": 12,         # Number of attention heads
     "n_layers": 12,        # Number of layers
     "drop_rate": 0.1,      # Dropout rate
-    "qkv_bias": False      # Query-key-value bias
+    "qkv_bias": False      # Query-key-value bias : 現代的なLLMはfalse 
 }
 
 torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
 model.eval();  # Disable dropout during inference
+
+# [3] In addition, we define two convenience functions, text_to_token_ids and token_ids_to_text, for converting between token and text representations that we use throughout this chapter
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (batch, n_tokens) array of indices in the current context
+    for _ in range(max_new_tokens):
+        
+        # Crop current context if it exceeds the supported context size
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
+        idx_cond = idx[:, -context_size:]
+        
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+        
+        # Focus only on the last time step
+        # (batch, n_tokens, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]  
+
+        # Apply softmax to get probabilities
+        probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size)
+
+        # Get the idx of the vocab entry with the highest probability value
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
+
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
+
+
+
+import tiktoken
+
+
+# Alternatively:
+# from llms_from_scratch.ch04 import generate_text_simple
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0) # remove batch dimension
+    return tokenizer.decode(flat.tolist())
+
+start_context = "Every effort moves you"
+tokenizer = tiktoken.get_encoding("gpt2")
+
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids(start_context, tokenizer),
+    max_new_tokens=10,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+
+# 5.1.2 Calculating the text generation loss: cross-entropy and perplexity
+
+# 1文字づつずらすことで次の正しいトークンを教える
+
+# [4]
+inputs = torch.tensor([[16833, 3626, 6100],   # ["every effort moves",
+                       [40,    1107, 588]])   #  "I really like"]
+
+targets = torch.tensor([[3626, 6100, 345  ],  # [" effort moves you",
+                        [1107,  588, 11311]]) #  " really like chocolate"]
+
+
+
+# [5]
+
+with torch.no_grad():
+    logits = model(inputs) # NNの最終出力vector
+
+probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary 確率分歩のベクトル
+print(probas.shape) # Shape: (batch_size, num_tokens, vocab_size)
+
+
+# [6] argmaxためすで
+
+token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+print("Token IDs:\n", token_ids)
+
+
+# [7] If we decode these tokens, we find that these are quite different from the tokens we want the model to predict, namely the target tokens:
+
+print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")
+print(f"Outputs batch 1: {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+
+
+# どれぐらい理想から離れてるかを知る必要がある。
+
+# [8] The token probabilities corresponding to the target indices are as follows:
+
+text_idx = 0
+target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 1:", target_probas_1)
+
+text_idx = 1
+target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 2:", target_probas_2)
+
+# [9] 最適化は、対数をつかうほうがいいことが知られている
+
+# Compute logarithm of all token probabilities
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+print(log_probas)
+
+# [10] Next, we compute the average log probability:
+
+# Calculate the average probability for each token
+avg_log_probas = torch.mean(log_probas)
+print(avg_log_probas)
+
+# The goal is to make this average log probability as large as possible by optimizing the model weights
+# この avg_log_probas を最大にするようなmodelの重みを求めたい
+# 最大は0
+
+# [11] The value negative of -10.7722, i.e., 10.7722, is also called cross-entropy loss in deep learning
+
+
+neg_avg_log_probas = avg_log_probas * -1
+print(neg_avg_log_probas)  # これが、cross-entropy loss と呼ばれている 10.7940
+
+# logits > Probabilities > target probabilities > log probabilities > average log probability > negative avg log probabs
+# 生出力 > softmaxで確率分布 > 正解の単語が選ばれる確率 > 対数にする > 平均 > -1をかける
+
+# つまり target probabilities がぜんぶ1になったら完璧ってことじゃな!
+# 正解の単語が選ばれる確率が probs で低かったらダメ。
+
+# PyTorch already implements a cross_entropy function that carries out the previous steps
+
+# [12] Before we apply the cross_entropy function, let's check the shape of the logits and targets
+
+# Logits have shape (batch_size, num_tokens, vocab_size)
+print("Logits shape:", logits.shape)
+
+# Targets have shape (batch_size, num_tokens)
+print("Targets shape:", targets.shape)
+
+# [13] flattenせな
+
+logits_flat = logits.flatten(0, 1)
+targets_flat = targets.flatten()
+
+print("Flattened logits:", logits_flat.shape)
+print("Flattened targets:", targets_flat.shape)
+
+# [14] loss
+
+loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+print(loss)
+
+
+
+# [15] The perplexity is simply the exponential of the cross-entropy loss
+
+perplexity = torch.exp(loss)
+print(perplexity)  # 迷いの度合い 48000ぐらい。約5万の語彙に対して。
+
+
 
