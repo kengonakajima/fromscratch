49b88fe2f2f711381a4c7fb99f85cb8687ad1634
kengo nakajima <kengo.nakajima@gmail.com>
2025-08-21 09:22:11 +0900

    7

--- ch07.py
+++ ch07.py
@@ -16,4 +16,323 @@ pkgs = [
 for p in pkgs:
     print(f"{p} version: {version(p)}")
 
-    
+import os
+import urllib.request
+
+
+# Pretraining → 「次の単語予測」に特化 → 補完はうまいけど、質問に答えるとか指示に沿うのは苦手。
+# Instruction finetuning → 「人間が書いた指示と回答のペア」を学習 → 指示に沿った出力ができるようになる。
+
+# 補足すると、ChatGPT や Claude みたいに「指示を理解して答える LLM」になるための大事なステップがこの Instruction finetuning です。    
+
+# 7.2 Preparing a dataset for supervised instruction finetuning
+
+
+# [2] dataset
+
+import json
+import os
+import urllib
+
+
+def download_and_load_file(file_path, url):
+
+    if not os.path.exists(file_path):
+        with urllib.request.urlopen(url) as response:
+            text_data = response.read().decode("utf-8")
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text_data)
+
+    # The book originally contained this unnecessary "else" clause:
+    #else:
+    #    with open(file_path, "r", encoding="utf-8") as file:
+    #        text_data = file.read()
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return data
+
+
+file_path = "instruction-data.json"
+url = (
+    "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch"
+    "/main/ch07/01_main-chapter-code/instruction-data.json"
+)
+
+data = download_and_load_file(file_path, url)
+print("Number of entries:", len(data))
+
+# [3]
+
+print("Example entry:\n", data[50])
+
+# [4]
+
+print("Another example entry:\n", data[999])
+
+
+# 「Alpaca style」とは
+# 研究コミュニティで「Alpaca style」というと、
+# 小さなシードデータ（人間が書いた少量の instruction–response ペア） を用意
+# それを使って 既存の強いモデル（例: GPT-3.5）から大量の疑似 instruction データを生成
+# そのデータでオープンモデル（例: LLaMA, Mistral, DeepSeek など）を finetune
+# という一連の手法を指します。
+
+# [5]
+def format_input(entry):
+    instruction_text = (
+        f"Below is an instruction that describes a task. "
+        f"Write a response that appropriately completes the request."
+        f"\n\n### Instruction:\n{entry['instruction']}"
+    )
+
+    input_text = f"\n\n### Input:\n{entry['input']}" if entry["input"] else ""
+
+    return instruction_text + input_text
+
+# [6]
+
+model_input = format_input(data[50])
+desired_response = f"\n\n### Response:\n{data[50]['output']}"
+
+print(model_input + desired_response)
+
+# [7] Below is a formatted response without an input field
+
+
+model_input = format_input(data[999])
+desired_response = f"\n\n### Response:\n{data[999]['output']}"
+
+print(model_input + desired_response)
+
+# [8]
+
+train_portion = int(len(data) * 0.85)  # 85% for training
+test_portion = int(len(data) * 0.1)    # 10% for testing
+val_portion = len(data) - train_portion - test_portion  # Remaining 5% for validation
+
+train_data = data[:train_portion]
+test_data = data[train_portion:train_portion + test_portion]
+val_data = data[train_portion + test_portion:]
+
+# [9]
+
+print("Training set length:", len(train_data))
+print("Validation set length:", len(val_data))
+print("Test set length:", len(test_data))
+
+
+# 7.3 Organizing data into training batches
+
+# [10]
+
+import torch
+from torch.utils.data import Dataset
+
+
+class InstructionDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+
+        # Pre-tokenize texts
+        self.encoded_texts = []
+        for entry in data:
+            instruction_plus_input = format_input(entry)
+            response_text = f"\n\n### Response:\n{entry['output']}"
+            full_text = instruction_plus_input + response_text
+            self.encoded_texts.append(
+                tokenizer.encode(full_text)
+            )
+
+    def __getitem__(self, index):
+        return self.encoded_texts[index]
+
+    def __len__(self):
+        return len(self.data)
+
+
+# [11]
+import tiktoken
+tokenizer = tiktoken.get_encoding("gpt2")
+
+print(tokenizer.encode("<|endoftext|>", allowed_special={"<|endoftext|>"}))
+
+# [12]
+
+def custom_collate_draft_1(
+    batch,
+    pad_token_id=50256,
+    device="cpu"
+):
+    # Find the longest sequence in the batch
+    # and increase the max length by +1, which will add one extra
+    # padding token below
+    batch_max_length = max(len(item)+1 for item in batch)
+
+    # Pad and prepare inputs
+    inputs_lst = []
+
+    for item in batch:
+        new_item = item.copy()
+        # Add an <|endoftext|> token
+        new_item += [pad_token_id]
+        # Pad sequences to batch_max_length
+        padded = (
+            new_item + [pad_token_id] *
+            (batch_max_length - len(new_item))
+        )
+        # Via padded[:-1], we remove the extra padded token
+        # that has been added via the +1 setting in batch_max_length
+        # (the extra padding token will be relevant in later codes)
+        inputs = torch.tensor(padded[:-1])
+        inputs_lst.append(inputs)
+
+    # Convert list of inputs to tensor and transfer to target device
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    return inputs_tensor
+
+# [13]
+
+inputs_1 = [0, 1, 2, 3, 4]
+inputs_2 = [5, 6]
+inputs_3 = [7, 8, 9]
+
+batch = (
+    inputs_1,
+    inputs_2,
+    inputs_3
+)
+
+print(custom_collate_draft_1(batch))
+
+# [14]
+
+def custom_collate_draft_2(
+    batch,
+    pad_token_id=50256,
+    device="cpu"
+):
+    # Find the longest sequence in the batch
+    batch_max_length = max(len(item)+1 for item in batch)
+
+    # Pad and prepare inputs
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        # Add an <|endoftext|> token
+        new_item += [pad_token_id]
+        # Pad sequences to max_length
+        padded = (
+            new_item + [pad_token_id] *
+            (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])  # Truncate the last token for inputs
+        targets = torch.tensor(padded[1:])  # Shift +1 to the right for targets
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    # Convert list of inputs to tensor and transfer to target device
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
+# [15]
+
+inputs, targets = custom_collate_draft_2(batch)
+print(inputs)
+print(targets)
+
+# 50256 は GPT 系モデルの end-of-text (EOT) トークン。
+# データをバッチに揃えるために、短い文には EOT を パディングの代わりとして埋め込む。
+# しかし、EOT が複数回並んでしまうと、「本当の文の終わり」なのか「ただのパディング」なのか区別できなくなる。
+# そこで：
+# 最初の EOT (文の終了) は残す
+# それ以降の EOT (パディング部分) は -100 に置き換える
+
+# [16]
+
+def custom_collate_fn(
+    batch,
+    pad_token_id=50256,
+    ignore_index=-100,
+    allowed_max_length=None,
+    device="cpu"
+):
+    # Find the longest sequence in the batch
+    batch_max_length = max(len(item)+1 for item in batch)
+
+    # Pad and prepare inputs and targets
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        # Add an <|endoftext|> token
+        new_item += [pad_token_id]
+        # Pad sequences to max_length
+        padded = (
+            new_item + [pad_token_id] *
+            (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])  # Truncate the last token for inputs
+        targets = torch.tensor(padded[1:])  # Shift +1 to the right for targets
+
+        # New: Replace all but the first padding tokens in targets by ignore_index
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+        # New: Optionally truncate to maximum sequence length
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    # Convert list of inputs and targets to tensors and transfer to target device
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+
+    return inputs_tensor, targets_tensor
+
+# [17]
+
+inputs, targets = custom_collate_fn(batch)
+print(inputs)
+print(targets)
+
+# [18]
+
+logits_1 = torch.tensor(
+    [[-1.0, 1.0],  # 1st training example
+     [-0.5, 1.5]]  # 2nd training example
+)
+targets_1 = torch.tensor([0, 1])
+
+
+loss_1 = torch.nn.functional.cross_entropy(logits_1, targets_1)
+print(loss_1)
+
+# [19]
+
+logits_2 = torch.tensor(
+    [[-1.0, 1.0],
+     [-0.5, 1.5],
+     [-0.5, 1.5]]  # New 3rd training example
+)
+targets_2 = torch.tensor([0, 1, 1])
+
+loss_2 = torch.nn.functional.cross_entropy(logits_2, targets_2)
+print(loss_2)
+
+# [20]
+
+targets_3 = torch.tensor([0, 1, -100])
+
+loss_3 = torch.nn.functional.cross_entropy(logits_2, targets_3)
+print(loss_3)
+print("loss_1 == loss_3:", loss_1 == loss_3) # ignore_index=-100が効いてる
+
